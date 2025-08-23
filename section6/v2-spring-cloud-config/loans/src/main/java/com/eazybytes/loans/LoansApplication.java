c184f667dc7bfcec26d377dd05c8423a7cb7eab7
Pato <75866080+warrior226@users.noreply.github.com>
2025-08-23 18:10:08 +0000

    Reading configurations from the class path location of Config Server

--- section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
+++ section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -1,5 +1,6 @@
 package com.eazybytes.loans;
 
+import com.eazybytes.loans.dto.LoansInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,9 +8,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LoansInfoDto.class)
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
         info = @Info(
