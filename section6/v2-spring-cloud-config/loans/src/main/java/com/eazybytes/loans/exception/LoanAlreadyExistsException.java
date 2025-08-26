16d690e1c4f1e03d6932996f67e005c2dbc3fb0d
Patrick RAYAISSE <75866080+warrior226@users.noreply.github.com>
2025-08-26 12:33:43 +0000

    Delete section6/v2-spring-cloud-config directory

--- section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/exception/LoanAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.eazybytes.loans.exception;
-
-public class LoanAlreadyExistsException extends RuntimeException {
-    public LoanAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.eazybytes.loans.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String loan, String mobileNumber, String mobileNumber1) {
-        super(String.format("%s not found with the given  %s : '%s'",loan,mobileNumber,mobileNumber1));
-    }
-}
