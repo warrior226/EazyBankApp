2e9fa6810ceba3d1ee1571094f1f6fc623d85c2a
Pato <75866080+warrior226@users.noreply.github.com>
2025-08-26 19:57:58 +0000

    Refresh configurations at runtime using Spring Cloud Bus

--- section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/dto/LoansInfoDto.java
+++ section6/v2-spring-cloud-config/loans/src/main/java/com/eazybytes/loans/dto/LoansInfoDto.java
@@ -1,11 +1,18 @@
 package com.eazybytes.loans.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansInfoDto{
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
